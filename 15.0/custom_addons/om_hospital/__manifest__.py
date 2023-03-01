b8fe8d75ab983ffc3ff776f28b15daf3eaeaf067
ka1bee <mansur17012004@gmail.com>
2023-03-01 17:38:58 +0600

    Initial commit

---
+++ 15.0/custom_addons/om_hospital/__manifest__.py
@@ -0,0 +1,17 @@
+
+{
+    'name': 'Hospital Management',
+    'version': '1.0.0',
+    'category': 'Hospital',
+    'author': 'Mansur R.',
+    'sequence': -100,
+    'summary': 'Hospital management system',
+    'description': """Hospital management system""",
+    'depends': [],
+    'data': [],
+    'demo':[],
+    'application': True,
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
